7ab6cc1d9142747d32ed3bd299ea4a066679f496
tiyan <tiyan@mamiteam.com>
2020-11-02 10:16:06 +0700

    first commit

---
+++ movieApi/src/commonMain/kotlin/com/example/movieapp/movieApi/Greeting.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.movieApi
+
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ movieApi/src/commonMain/kotlin/com/example/movieapp/movieApi/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.movieapp.movieApi
+
+expect class Platform() {
+    val platform: String
+}
